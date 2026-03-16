d8d15bde524d5547cce9feeadffaeed22852f9a2
Rebeca <rebecamartinezmedina@hotmail.com>
2026-03-16 16:05:06 +0100

    Importaciones

--- noverifactu-back/routes/usuario.routes.js
+++ noverifactu-back/routes/usuario.routes.js
@@ -6,6 +6,7 @@ import { registrarEvento } from "../utils/eventos.js";
 import fs from "fs";
 import { uploadLogo } from "../middleware/upload.js";
 import { PLANES } from "../utils/planes.js";
+import { r2 } from "../utils/r2.js";
 import { DeleteObjectCommand } from "@aws-sdk/client-s3";
 
 const router = express.Router();
